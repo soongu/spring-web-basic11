7e6a51005b6ba0bd4120fe6d29a078311f9b10a0
soongu <naong526@naver.com>
2022-12-24 14:06:23 +0900

    #21-2. 배포 설정 완료

--- src/main/java/com/example/demo/HealthyCheckController.java
+++ src/main/java/com/example/demo/HealthyCheckController.java
@@ -2,17 +2,25 @@ package com.example.demo;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // 백엔드 서버가 살아있는지 확인
 @RestController
 @Slf4j
+@CrossOrigin
 public class HealthyCheckController {
 
     @GetMapping("/")
     public String check() {
         log.info("server is running...");
         return "server is running....";
+    }
+
+    @GetMapping("/hello123")
+    public String check123() {
+        log.info("hello123...");
+        return "안뇽안뇽 하하호호~~";
     }
 }
